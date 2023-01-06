066e630e9958238104d5fa1cdc3570e1fae84c47
Maciej Rzadzinski <maciej.rzd@gmail.com>
2023-01-06 19:12:03 +0100

    Solve fibonacci iterative

---
+++ fibonacci/fibonacci-iterative.js
@@ -0,0 +1,30 @@
+/* 
+Instruction:
+Using iteration, write a function which takes a number and returns an array 
+containing that many numbers from the fibonacci sequence. Using an example 
+input of 8, this method should return the array [0, 1, 1, 2, 3, 5, 8, 13].
+*/
+
+// Using zero as a first number of the sequence
+function fibIter(n) {
+    if (n <= 0 || !Number.isInteger(n)) {
+        return console.log('Please provide positive integer.')
+    }
+    if (n === 1) return [0];
+
+    let arr = [0, 1];
+    let first = 0;
+    let second = 1;
+
+    for (let i = 2; i < n; i++) {
+        arr.push(first + second);
+        first = second;
+        second = arr[arr.length - 1];
+    }
+    return arr;
+}
+
+console.log(fibIter(-2));
+console.log(fibIter(1));
+console.log(fibIter(8));
+console.log(fibIter(13));
